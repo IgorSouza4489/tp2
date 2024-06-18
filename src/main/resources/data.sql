6c539ebddafcef9651b09a1357287c173b1e4dd5
Vitor Ferreira Lopes <100311898+vitorflopes@users.noreply.github.com>
2024-06-18 18:22:39 -0300

    mais testes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,6 @@ INSERT INTO usuario (id, nome, email, senha, is_doctor, especialidade) VALUES
 
 -- Insert data into Consulta table
 INSERT INTO consultas (id, data, id_paciente, id_medico, consulta_finalizada, diagnostico) VALUES
-(1, '2023-12-12', 1, 2, false, '3 dias de vida'),
-(2, '2023-12-13', 1, 3, true, 'Consulta de retorno'),
-(3, '2023-12-14', 2, 3, false, 'Consulta de rotina');
+(2, '2023-12-12', 1, 2, false, '3 dias de vida'),
+(3, '2023-12-13', 1, 3, true, 'Consulta de retorno'),
+(4, '2023-12-14', 2, 3, false, 'Consulta de rotina');
--- src/test/java/com/example/tp2/ConsultaServiceTests.java
+++ src/test/java/com/example/tp2/ConsultaServiceTests.java
@@ -25,16 +25,62 @@ public class ConsultaServiceTests {
 
         Consulta consulta = new Consulta();
         consulta.setData(LocalDate.of(2024, 6, 17));
-        consulta.setIdPaciente(1);
-        consulta.setIdMedico(1);
+        consulta.setPaciente(null);
+        consulta.setMedico(null);
         consulta.setConsultaFinalizada(true);
         consulta.setDiagnostico("Gripe");
-
         consultaService.save(consulta);
 
         all = consultaService.getAll();
         int estadoFinal = all.size();
 
         assertEquals(estadoInicial+1, estadoFinal);
+    }
+
+    @Test
+    @DisplayName("Deve deletar uma consulta do banco.")
+    public void testarDelete() {
+        List<Consulta> all = consultaService.getAll();
+        int estadoInicial = all.size();
+
+        consultaService.deleteById(2);
+
+        all = consultaService.getAll();
+        int estadoFinal = all.size();
+
+        assertEquals(estadoInicial-1, estadoFinal);
+    }
+
+    @Test
+    @DisplayName("Deve atualizar uma consulta existente no banco.")
+    public void testarUpdate() {
+        int id_consulta = 3;
+        Consulta consultaParaAtualizar = consultaService.getById(id_consulta);
+        consultaParaAtualizar.setDiagnostico("Dengue");
+        consultaService.update(id_consulta, consultaParaAtualizar);
+
+        Consulta consultaAtualizada = consultaService.getById(id_consulta);
+
+        assertEquals("Dengue", consultaAtualizada.getDiagnostico());
+    }
+
+    @Test
+    @DisplayName("Deve buscar uma consulta por ID.")
+    public void testarGetById() {
+        int id_consulta = 2;
+
+        Consulta consulta = new Consulta();
+        consulta.setId(id_consulta);
+        consulta.setData(LocalDate.of(2024, 6, 18));
+        consulta.setPaciente(null);
+        consulta.setMedico(null);
+        consulta.setConsultaFinalizada(true);
+        consulta.setDiagnostico("Frescura");
+        consultaService.save(consulta);
+
+        Consulta consultaCadastrada = consultaService.getById(id_consulta);
+
+        assertEquals(consulta.getId(), consultaCadastrada.getId());
+        assertEquals(consulta.getDiagnostico(), consultaCadastrada.getDiagnostico());
     }
 }
